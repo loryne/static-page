e3cd27256269af204bead38b3cda5e1ceb0031df
alladin0009 <alladin0009>
2018-11-13 08:32:40 +0300

    initial

---
+++ application/views/pages/about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" css/style.css">
+<script type='text/javascript' src="js/jquery.min.js"></script>
+<title>The Best</title>
+</head>
+<body>
+<div class="container">
+  <img src=("images/2.png")>
+  <h3 style="color:green;">LAURA</h3>
+  <h3 style="color:blue;">LORYNE</h3>
+  <h3 style="color:pink;">GRACE</h3>
+</div>
+
+</body>
+</html>
